edc811ccea1bb406b891eeb1618eb92238b39f70
Thesiri04 <sirirungnapa201@gmail.com>
2025-03-17 22:13:57 +0700

    แบบฝึก

---
+++ Lab17_Ex04/Program.cs
@@ -0,0 +1,74 @@
+﻿interface IMovable
+{
+    public void Move();
+}
+
+interface ISpeakable
+{
+    public void Speak();
+}
+
+abstract class Animal : IMovable, ISpeakable
+{
+    // abstract method for Move
+    public abstract void Move();
+
+    // abstract method for Speak that subclasses must implement
+    public abstract void Speak();
+}
+
+class Dog : Animal
+{
+    public override void Move()
+    {
+        System.Console.WriteLine("Dog move by running on the ground");
+    }
+
+    public override void Speak()
+    {
+        System.Console.WriteLine("Dog barks");
+    }
+}
+
+class Fish : Animal
+{
+    public override void Move()
+    {
+        System.Console.WriteLine("Fish move by swimming in the water");
+    }
+
+    public override void Speak()
+    {
+        System.Console.WriteLine("Fish makes bubbling sounds");
+    }
+}
+
+class Bird : Animal
+{
+    public override void Move()
+    {
+        System.Console.WriteLine("Bird move by flying in the air");
+    }
+
+    public override void Speak()
+    {
+        System.Console.WriteLine("Bird chirps");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Animal[] animals = new Animal[3];
+        animals[0] = new Dog();
+        animals[1] = new Bird();
+        animals[2] = new Fish();
+
+        foreach (var animal in animals)
+        {
+            animal.Move(); // Calling Move method
+            animal.Speak(); // Calling Speak method
+        }
+    }
+}
---
+++ Lab17_Ex06/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+interface IRemoteControl
+{
+    public void TurnOn();
+    public void TurnOff();
+    public void ChannelUp();
+    public void ChannelDown();
+}
+
+abstract class PowerAppliance
+{
+    public bool PowerStatus;
+    public int Wattage;
+}
+
+abstract class Television : PowerAppliance
+{
+    public int Channel { get; set; }
+}
+
+abstract class Lamp : PowerAppliance
+{
+}
+
+class SonyTV : Television, IRemoteControl
+{
+    public void TurnOn() { System.Console.WriteLine("TV Turn on"); PowerStatus = true; }
+    public void TurnOff() { System.Console.WriteLine("TV Turn off"); PowerStatus = false; }
+    public void ChannelUp() { System.Console.WriteLine("TV Channel up"); }
+    public void ChannelDown() { System.Console.WriteLine("TV Channel down"); }
+}
+
+class DesktopLamp : Lamp, IRemoteControl
+{
+    public void TurnOn() { System.Console.WriteLine("Lamp Turn on"); PowerStatus = true; }
+    public void TurnOff() { System.Console.WriteLine("Lamp Turn off"); PowerStatus = false; }
+    public void ChannelUp() { System.Console.WriteLine("Lamp cannot change channel"); }
+    public void ChannelDown() { System.Console.WriteLine("Lamp cannot change channel"); }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Use the specific classes that implement IRemoteControl
+        IRemoteControl myTV = new SonyTV();
+        myTV.TurnOn();
+        myTV.ChannelUp();
+        myTV.ChannelDown();
+        myTV.TurnOff();
+
+        IRemoteControl myLamp = new DesktopLamp();
+        myLamp.TurnOn();
+        myLamp.ChannelUp();
+        myLamp.ChannelDown();
+        myLamp.TurnOff();
+    }
+}
